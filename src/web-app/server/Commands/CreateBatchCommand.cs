988e44ff1c17eacd8866527a3c10bb8e6d8f0dfe
Trenton Hicks <trentonmhicks@gmail.com>
2020-08-20 13:48:07 -0500

    Added server-side validation when creating a batch.

--- src/web-app/server/Commands/CreateBatchCommand.cs
+++ src/web-app/server/Commands/CreateBatchCommand.cs
@@ -1,12 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodeJar.WebApp.Commands
 {
     public class CreateBatchCommand
     {
-        public string BatchName {get; set;}
-        public int BatchSize {get; set;}
-        public DateTime DateActive {get; set;}
-        public DateTime DateExpires {get; set;}
+        [Required]
+        [StringLength(50)]
+        public string BatchName { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public int BatchSize { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PromotionType { get; set; }
+
+        [Required]
+        public DateTime DateActive { get; set; }
+
+        [Required]
+        public DateTime DateExpires { get; set; }
     }
 }
